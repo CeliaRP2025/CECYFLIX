ec9db45e37ff18ccb9e8f5ff252c9e6ebefac62c
CeliaRP2025 <celiaromeropichardo@gmail.com>
2025-07-21 00:24:49 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
     setPeliculasFiltradas([]);
 
     try {
-      const response = await fetch('/api/recomendaciones', {
+      const response = await fetch('https://recomendaciones-backend.onrender.com/api/recomendaciones', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
